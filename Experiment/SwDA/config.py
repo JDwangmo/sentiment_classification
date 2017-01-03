f941825aa492f151b275cd742817468c0037e938
jdwang <383287471@qq.com>
2017-01-03 23:23:56 +0800

    update SwDA

---
+++ Experiment/SwDA/config.py
@@ -0,0 +1,10 @@
+# encoding=utf8
+"""
+    Author:  'jdwang'
+    Date:    'create date: 2017-01-03'; 'last updated date: 2017-01-03'
+    Email:   '383287471@qq.com'
+    Describe:
+"""
+from __future__ import print_function
+
+from dataset.SwDA_26.swda.swda import *
---
+++ dataset/SwDA_26/__init__.py
@@ -0,0 +1,8 @@
+# encoding=utf8
+"""
+    Author:  'jdwang'
+    Date:    'create date: 2017-01-03'; 'last updated date: 2017-01-03'
+    Email:   '383287471@qq.com'
+    Describe:
+"""
+from __future__ import print_function
